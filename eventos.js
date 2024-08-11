81f8f87795187f3e1c5387b38475421f6472a2e4
sofiacurbeloo <sofiacurbelo923@gmail.com>
2024-08-11 17:04:05 -0300

    evento agregado

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let div = document.getElementById("button_div");
+    div.addEventListener("click", function() {
+    
+        alert("Hola! soy el div");
+    
+});
+})
