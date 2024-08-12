97ec8d2137e144985cc25a040ac4fac5b0ca83b4
sofiacurbeloo <sofiacurbelo923@gmail.com>
2024-08-11 23:23:31 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     let div = document.getElementById("button_div");
+    let btn = document.querySelector("button");
+
     div.addEventListener("click", function() {
-    
         alert("Hola! soy el div");
+
+    });
+
     
-});
+    btn.addEventListener("click", function(event) {
+        alert("Hola!");
+        event.stopPropagation();
+    });
+
+
 })
